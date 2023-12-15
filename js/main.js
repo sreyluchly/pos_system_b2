de375cf4b9f0e01ec0028237efd195a8ca530573
MENGKORNG.LY <MENGKORNG.LY@PNC.LAN>
2023-12-15 20:01:14 +0700

    pull code

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ const main = document.querySelector('main');
 const aside = document.querySelector('aside');
 const formAdd = document.querySelector('.form-container');
 const btnAdd = document.getElementById('btnAdd');
-let arrayImage = [
-    {id: 1, img: "IMG/image1.jpg", name: "Ash", price: 10, category: "american"},
-    {id: 2, img: "IMG/image2.jpg", name: "Spaghetti", price: 10, category: "italy"},
-    {id: 3, img: "IMG/image3.jpg", name: "Chinese Noodles", price: 5, category: "other"},
-    {id: 4, img: "IMG/image4.jpg", name: "Sushi", price: 100, category: "other"},
-    {id: 5, img: "IMG/image5.jpg", name: "Fish Fried", price: 35, category: "american"},
-    {id: 6, img: "IMG/image6.jpg", name: "Samon Fried", price: 15, category: "american"},
-    {id: 7, img: "IMG/image7.jpg", name: "Burger", price: 5, category: "american"},
-    {id: 8, img: "IMG/image8.jpg", name: "Prawns", price: 50, category: "american"},
-    {id: 9, img: "IMG/image9.jpg", name: "Khmer Noodles", price: 5, category: "khmer"}
+const imgDetail = document.getElementById("imgDetail");
+let proId = 0;
+
+let arrayProduct = [
+    {id: 1, img: "IMG/image1.jpg", name: "Ash", quantity: 10, price: 10, category: "american"},
+    {id: 2, img: "IMG/image2.jpg", name: "Spaghetti", quantity: 15, price: 10, category: "italy"},
+    {id: 3, img: "IMG/image3.jpg", name: "Chinese Noodles", quantity: 20, price: 5, category: "other"},
+    {id: 4, img: "IMG/image4.jpg", name: "Sushi", quantity: 5, price: 100, category: "other"},
+    {id: 5, img: "IMG/image6.jpg", name: "Samon Fried", quantity: 15, price: 15, category: "american"},
+    {id: 6, img: "IMG/image9.jpg", name: "Khmer Noodles", quantity: 100, price: 5, category: "khmer"}
 ];
 let arrayDetail = [];
 // Show and hide Elements
@@ -25,46 +25,54 @@ function show(element) {
 }
 
 function saveStorage() {
-    localStorage.setItem("arrayImage", JSON.stringify(arrayImage));
+    localStorage.setItem("arrayProduct", JSON.stringify(arrayProduct));
 }
 function loadStorage() {
-    if (localStorage.getItem("arrayImage") != null) {
-        arrayImage = JSON.parse(localStorage.getItem("arrayImage"));
+    if (localStorage.getItem("arrayProduct") != null) {
+        arrayProduct = JSON.parse(localStorage.getItem("arrayProduct"));
     }
 }
 
+function deletPro(index) {
+    arrayProduct.splice(index, 1);
+    saveStorage();
+    showProduct();
+}
+
+function addNewProduct() {
+    let newProduct = {
+        id : arrayProduct[arrayProduct.length - 1].id + 1 ,
+        img: `${document.getElementById('urlPro').value}`,
+        name: `${document.getElementById("namePro").value}`,
+        quantity: `${document.getElementById("qtyPro").value}`,
+        price: `${document.getElementById("pricePro").value}`,
+        category: `${document.getElementById("cate").value}`
+    }
+    arrayProduct.push(newProduct);
+    saveStorage();
+    showProduct();
+    productDetail();
+    hide(formAdd);
+}
 function productDetail(event) {
-    let object = {};
-    let index = event.target.closest('.card').dataset.index;
-    let totalResult = 0;
-    object.name = arrayImage[index].name;
-    object.price = arrayImage[index].price;
-    arrayDetail.push(object);
-
-    for (const tr of document.querySelectorAll('tbody tr')) {
-        tr.remove()
+    proId = event != 0 ? event.target.parentElement.parentElement.dataset.index : 0;
+    imgDetail.src = arrayProduct[proId].img;
+    document.getElementById("proName").textContent = arrayProduct[proId].name;
+    document.getElementById("proCate").textContent = arrayProduct[proId].category;
+    document.getElementById("proPrice").textContent = arrayProduct[proId].price;
+    document.getElementById("delete").onclick = () => {
+        if (window.confirm("Are you sure you want to delete this product?")) {
+            document.getElementById("delete").setAttribute("onclick", `${deletPro(proId)}`)
+        }
     }
-    for (const pro of arrayDetail) {
-        const tr = document.createElement('tr');
-        const tdName = document.createElement('td');
-        const tdPrice = document.createElement('td');
-    
-        tdPrice.textContent = pro.price + "$";
-        tdName.textContent = pro.name;
-        tr.appendChild(tdName);
-        tr.appendChild(tdPrice);
-        tbody.appendChild(tr);
-        totalResult += pro.price;
-    }
-    document.getElementById('total').textContent = totalResult.toFixed(2) + "$";
-    document.getElementById('subtotal').textContent = (Math.round(totalResult)).toFixed(2) + "$";
 }
+
 // Show Card
-function showCard() {
+function showProduct() {
     for (const card of document.querySelectorAll(".card")) {
         card.remove();
     }
-    for (let i=0; i<arrayImage.length; i++) {
+    for (let i=0; i<arrayProduct.length; i++) {
         const card = document.createElement('div');
         const cardHeader = document.createElement('div');
         const cardFooter = document.createElement('div');
@@ -78,9 +86,9 @@ function showCard() {
         cardFooter.className = "card-footer";
         foodName.className = "product-name";
     
-        img.src = arrayImage[i].img;
-        foodName.textContent = arrayImage[i].name;
-        price.textContent = arrayImage[i].price + "$"; 
+        img.src = arrayProduct[i].img;
+        foodName.textContent = arrayProduct[i].name;
+        price.textContent = arrayProduct[i].price + "$"; 
         
         cardHeader.appendChild(img);
         cardFooter.appendChild(foodName);
@@ -100,17 +108,9 @@ btnAdd.onclick = () => {
     show(formAdd);
 }
 
-// Menu proccess
-for (const li of document.querySelectorAll('ul li')) {
-    li.onclick = () => {
-        for (const li of document.querySelectorAll('ul li')) {
-            li.className = ""
-        }
-        li.className = "after";
-    }
+function cancel() {
+    hide(formAdd);
 }
-
-
 
 // Button Dark and Light mode
 const btnTheme = document.querySelector(".nav-right button");
@@ -127,4 +127,6 @@ btnTheme.onclick = () => {
 }
 
 loadStorage();
-showCard();
+showProduct();
+productDetail();
+// localStorage.clear();
